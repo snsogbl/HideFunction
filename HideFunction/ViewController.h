3f5efd71f9510e96dc389b8944e09e5863ee7ba1
snsogbl <snsogbl1@163.com>
2015-04-15 13:55:44 +0800

    first

---
+++ HideFunction/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HideFunction
+//
+//  Created by a on 15/4/15.
+//  Copyright (c) 2015年 a. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HideFunction/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HideFunction
+//
+//  Created by a on 15/4/15.
+//  Copyright (c) 2015年 a. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
